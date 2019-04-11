b3ec19c42e321242e78488687217b2beee8ac1c9
zy_2071 <zy2071@126.com>
2019-04-11 17:04:09 +0800

    Edit

--- src/router.js
+++ src/router.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
-import Page1 from './routes/IndexPage1';
-import Page2 from './routes/IndexPage2';
-import Page3 from './routes/IndexPage3';
 import main from './routes';
 
 function RouterConfig({ history }) {
